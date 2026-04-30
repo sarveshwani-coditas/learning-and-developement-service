a14e41e1ad43e3701f2f5f36f404b1d768b33970
Sarvesh Wani <sarvesh.wani@coditas.com>
2026-04-30 15:07:58 +0530

    Initial commit

---
+++ src/main/java/com/coditas/learninganddevelopementservice/LearningAndDevelopementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coditas.learninganddevelopementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningAndDevelopementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningAndDevelopementServiceApplication.class, args);
+    }
+
+}
